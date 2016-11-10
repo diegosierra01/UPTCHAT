6be428c7baf95b2e21e889e5e48e07c8b7323abd
bagm009 <bray2909ing@gmail.com>
2016-11-10 11:26:44 -0500

    archivos bases del proyecto

---
+++ application/controllers/chat.php
@@ -0,0 +1,73 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class chat extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->model("chatmodel");
+    }
+
+    public function newId() {
+        
+       $cadena =  $this->chatmodel->getUltimo();
+       $numero = intval(preg_replace('/[^0-9]+/', '', $cadena), 10);
+       $semiID = $numero + 1;
+       $idAuto = 'U'.$semiID;       
+       return $idAuto;
+       
+    }
+    public function encriptar($cadena){
+        $key='';  
+        $encrypted = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $cadena, MCRYPT_MODE_CBC, md5(md5($key))));
+        return $encrypted; 
+
+    }
+    public function desencriptar($cadena) {
+            $key = ''; 
+            $decrypted = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($key), base64_decode($cadena), MCRYPT_MODE_CBC, md5(md5($key))), "\0");
+            return $decrypted; 
+    }
+
+    public function registrar() {
+     
+        $data['id'] = $this->newId();
+        $data['nick'] = $this->input->post("nick");
+        $data['password'] = $this->input->post("password");
+       
+        $this->chatmodel->insertarUsuario($data);
+        echo "Registrado exitosamente.";
+        
+    }
+   
+    public function autenticar() {
+        $nick = $this->input->get("nick");
+        $password = $this->input->get("password");
+        $user = $this->chatmodel->authUser($nick,$password);
+        echo json_encode(array('usuario' => $user));
+    }
+    public function enviarMensaje() {
+        $data['cadena'] = $this->input->post("cadena");
+        $data['remitente'] = $this->input->post("remitente");
+        $data['destinatario'] = $this->input->post("destinatario");
+        $data['fecha'] = $this->input->post("fecha");
+        $data['hora'] = $this->input->post("hora");
+       
+        $this->chatmodel->insertarMensaje($data);
+        echo "Registrado exitosamente.";
+        
+    }
+    public function listarUsuarios() {
+       
+        $users = $this->chatmodel->listaUsuarios();
+        echo json_encode(array('usuarios' => $users));
+    }
+    public function listarMensajes() {
+        $destinatario = $this->input->get("destinatario");
+        $mensajes= $this->chatmodel->listarMensajes($destinatario);
+        echo json_encode(array('mensajes' => $mensajes));
+    }
+
+}
---
+++ application/models/chatmodel.php
@@ -0,0 +1,51 @@
+<?php
+
+class chatmodel extends CI_Model {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+    public function authUser($nick,$password){
+       $data = $this->db->query("SELECT * from usuario WHERE nick = '".$nick ."' AND password = '".$password."' ");
+        if ($data->num_rows() > 0) {
+            $data = $data->result();
+            return $data;
+        } else {
+            return FALSE;
+        }
+    }
+     public function getUltimo(){
+       $query = $this->db->query("SELECT id FROM usuario Order by id Desc limit 1"); 
+       $row = $query->row(0);       
+       return $row->id;     
+    }
+   
+    public function insertarUsuario($data) {
+        $this->db->insert('usuario', $data);
+    }
+     public function insertarMensaje($data) {
+        $this->db->insert('mensaje', $data);
+    }
+    public function listaUsuarios(){
+       $data = $this->db->query("Select id,nick from usuario");
+        if ($data->num_rows() > 0) {
+            $data = $data->result();
+            return $data;
+        } else {
+            return FALSE;
+        }
+    }
+    public function listarMensajes($destinatario){
+        //SELECT cadena,DATE_FORMAT(fecha, '%H:%I:%S')as fecha  FROM mensaje WHERE destinatario = 'U2'
+       $data = $this->db->query("SELECT cadena, hora  FROM mensaje WHERE destinatario = '".$destinatario ."' ");
+        if ($data->num_rows() > 0) {
+            $data = $data->result();
+            return $data;
+        } else {
+            return FALSE;
+        }
+    }
+    
+
+}
